ebe20798ec1893bd930565f95d8fa7ad6e839f9f
Verônica A Silva <veh.silva@gmail.com>
2020-06-26 14:01:43 -0300

    Implementando autenticação

--- gestaoclientes/urls.py
+++ gestaoclientes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('djangoadmin/', admin.site.urls),
-    path('', include('clientes.urls'))
+    path('', include('clientes.urls')),
+    path('auth/', include ('autenticacao.urls')),
 ]
