5b8a9e1e0d1c085c496890bc9abac104461a7e8b
Verônica A Silva <veh.silva@gmail.com>
2020-06-29 11:27:36 -0300

    Configurações p deploy Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gestaoclientes.wsgi --log-file -
--- gestaoclientes/settings.py
+++ gestaoclientes/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c7li-0pm52k-=qc!6grya3d4it^g-%^wjinlqyhn^^c0djfl4*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestaocliente-veronica.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -76,13 +78,9 @@ WSGI_APPLICATION = 'gestaoclientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -124,10 +122,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_ROOT = 'media'
 
 #LOGIN
-#login
 LOGIN_URL = 'auth/login/'
 LOGIN_REDIRECT_URL = 'listar_clientes'
